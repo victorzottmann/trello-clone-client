88d732f0002b16824f350ab933843fbf163bbd59
victorzottmann <zottmann.victor@gmail.com>
2021-06-24 12:13:09 +1000

    create add card feature and integrate it with the backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
 import { useState, useEffect } from 'react';
-import { Button, Typography, Box } from '@material-ui/core';
-import axios from 'axios'
+import { Button, Typography, Box, Card, TextField } from '@material-ui/core';
 
 import './App.css';
+import { api } from './data'
  
-const apiUrl = process.env.REACT_APP_API_URL
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+
   useEffect(() => {
-    axios.get(`${apiUrl}/cards`)
+    api.get("/cards")
       .then(({ data }) => setCards(data))
   },[])
 
-  const [cards, setCards] = useState([])
+  const addCard = async (event) => {
+    event.preventDefault()
+    
+    // send a POST request to add the card to the backend
+    try {
+      const { data } = await api.post("/cards", {
+        title,
+        description
+      })
+
+      setTitle("")
+      setDescription("")
+
+      // update the component state with the new card
+      // make a clone of cards from state
+      const cardsClone = [...cards]
+      // add the new card
+      cardsClone.push({
+        id: data.id,
+        title: data.title,
+        description: data.description
+      })
+      // set it as the new state
+      setCards(cardsClone)
+    } catch (err) {
+      console.log("oops, something went wrong:", err)
+    }
+  }
+
+  const handleTextChange = (event, setter) => {
+    setter(event.target.value)
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Typography>trello clone</Typography>
-        <Button>This is a button</Button>
-        {cards.map(({ title, description, id }) => (
-          <Box key={id}>
+    <Box>
+      <Typography>trello clone</Typography>
+      {cards.map(({ title, description, id }) => (
+        <Box key={id}>
+          <Card>
             <Typography>title: {title}</Typography>
             <Typography>description: {description}</Typography>
-          </Box>
-        ))}
-      </header>
-    </div>
-  );
+          </Card>
+        </Box>
+      ))}
+      {/* todo: add a form to add a new card
+      - title input
+      - description input
+      - submit button */}
+      <form onSubmit={addCard}>
+        <TextField onChange={(e) => handleTextChange(e, setTitle)} value={title} id="title" label="Title" />
+        <TextField onChange={(e) => handleTextChange(e, setDescription)} value={description} id="description" label="Description" />
+        <Button type="submit">Add card</Button>
+      </form>
+    </Box>
+  )
 }
 
 export default App;
