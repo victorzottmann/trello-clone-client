31d613479004f0c269107e75a42af652a7125b80
victorzottmann <zottmann.victor@gmail.com>
2021-06-26 20:36:59 +1000

    add production backend URL to api config

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,67 @@
-import { useState, useEffect } from 'react';
-import { Button, Typography, Box, Card, TextField } from '@material-ui/core';
+import { useState, useEffect } from 'react'
+import {
+  Button,
+  Typography,
+  Box,
+  Card,
+  CardContent,
+  TextField,
+} from '@material-ui/core'
 
-import './App.css';
+import './App.css'
 import { api } from './data'
- 
+import { validateInput } from './utils/validators'
 
 function App() {
   const [cards, setCards] = useState([])
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
 
   useEffect(() => {
-    api.get("/cards")
-      .then(({ data }) => setCards(data))
-  },[])
+    api.get('/cards').then(({ data }) => setCards(data))
+  }, [])
 
   const addCard = async (event) => {
     event.preventDefault()
-    
+
     // send a POST request to add the card to the backend
     try {
-      const { data } = await api.post("/cards", {
+      const { data } = await api.post('/cards', {
         title,
-        description
+        description,
       })
 
-      setTitle("")
-      setDescription("")
+      setTitle('')
+      setDescription('')
 
       // update the component state with the new card
       // make a clone of cards from state
       const cardsClone = [...cards]
+
       // add the new card
       cardsClone.push({
         id: data.id,
         title: data.title,
-        description: data.description
+        description: data.description,
       })
+
       // set it as the new state
       setCards(cardsClone)
     } catch (err) {
-      console.log("oops, something went wrong:", err)
+      console.log('oops, something went wrong:', err)
     }
   }
 
   const handleTextChange = (event, setter) => {
+    // run the input through the validator function
+    const textInput = event.target.value
+    if (validateInput(textInput)) {
+      setter(textInput)
+    } else {
+      alert('That is a naughty word!')
+    }
+    // if valid, update state
+    // if invalid, throw an error
     setter(event.target.value)
   }
 
@@ -53,23 +70,32 @@ function App() {
       <Typography>trello clone</Typography>
       {cards.map(({ title, description, id }) => (
         <Box key={id}>
-          <Card>
-            <Typography>title: {title}</Typography>
-            <Typography>description: {description}</Typography>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography>title: {title}</Typography>
+              <Typography>description: {description}</Typography>
+            </CardContent>
           </Card>
         </Box>
       ))}
-      {/* todo: add a form to add a new card
-      - title input
-      - description input
-      - submit button */}
+
       <form onSubmit={addCard}>
-        <TextField onChange={(e) => handleTextChange(e, setTitle)} value={title} id="title" label="Title" />
-        <TextField onChange={(e) => handleTextChange(e, setDescription)} value={description} id="description" label="Description" />
+        <TextField
+          onChange={(e) => handleTextChange(e, setTitle)}
+          value={title}
+          id="title"
+          label="Title"
+        />
+        <TextField
+          onChange={(e) => handleTextChange(e, setDescription)}
+          value={description}
+          id="description"
+          label="Description"
+        />
         <Button type="submit">Add card</Button>
       </form>
     </Box>
   )
 }
 
-export default App;
+export default App
---
+++ src/utils/validators.js
@@ -0,0 +1,10 @@
+export const validateInput = (input) => {
+  const bannedWords = ['bum']
+  let valid = true
+  bannedWords.forEach((word) => {
+    if (input.includes(word)) {
+      valid = false
+    }
+  })
+  return valid
+}
