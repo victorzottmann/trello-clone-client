fc07d70c426907db0fdda6d818ef47ca4c176efb
victorzottmann <zottmann.victor@gmail.com>
2021-07-06 23:23:53 +1000

    customize navbar with material ui and configure links to route to the correct paths

--- src/scenes/nav/Navbar.jsx
+++ src/scenes/nav/Navbar.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { AppBar, Tabs, Tab, Toolbar, Typography } from '@material-ui/core'
+
+const navLinks = [
+  {
+    label: 'Home',
+    value: '/',
+  },
+  {
+    label: 'Sign in',
+    value: '/sign-in',
+  },
+  {
+    label: 'Sign up',
+    value: '/sign-up',
+  },
+]
 
 export const Navbar = () => {
+  const { pathname } = useLocation()
+
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/sign-in">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/sign-up">Sign Up</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography component="h1" variant="h6">
+          trello clone
+        </Typography>
+
+        <Tabs value={pathname}>
+          {navLinks.map(({ label, value }) => (
+            <Tab
+              key={value}
+              label={label}
+              value={value}
+              component={Link}
+              to={value}
+            />
+          ))}
+        </Tabs>
+      </Toolbar>
+    </AppBar>
   )
 }
